562b4904f892bd63271eab60d645fbf48734effe
GabrielDesmodus <50773849+GabrielDesmodus@users.noreply.github.com>
2019-06-13 04:59:16 -0300

    Add files via upload

---
+++ pol3.js
@@ -0,0 +1,8 @@
+function toggle_visibility(caos) {
+       var e = document.getElementById(caos);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+           e.style.display = 'block';
+    }
+    
